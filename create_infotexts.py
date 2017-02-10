229dfeda4b26dc4a711def7b98f38b114955d46b
Mattias Östmar <mattiasostmar@gmail.com>
2017-02-10 14:15:33 +0100

    add function load_json_metadata()

--- create_infotexts.py
+++ create_infotexts.py
@@ -1,42 +1,20 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""<HEADER OF YOUR MODULE DOCSTRING>.
-Module-level docstrings appear as the first "statement" in a module. Remember,
-that while strings are regular Python statements, comments are not, so an
-inline comment may precede the module-level docstring.
-After importing a module, you can access this special string object through the
-``__doc__`` attribute; yes, it's actually available as a runtime attribute,
-despite not being given an explicit name! The ``__doc__`` attribute is also
-what is rendered when you call ``help()`` on a module, or really any other
-object in Python.
-You can also document a package using the module-level docstring in the
-package's ``__init__.py`` file.
+"""Create info files for batch SMVK-Cypern_2017-01
+This is step 2 in the batchupload process for SMVK-Cypern_2017-01. Step 1. was to transform the Excel-metadata to
+JSON-file ´SMVK-Cypern_2017-01_metadata.json´ and add new filenames, SMVK-MM-link and add Fotonummer as key..
+
+Outputs <Fotonummer>.info files in a subdirectory ./infofiles/
 """
 
 import os
-import batchupload.helpers as helpers
-
-class ImageFile(object, original_fname, original_path):
-    """One imagefile to be uploaded to Wikimedia Commons within this specific batchupload.
-
-        Attributes:
-            original_fname: A string representing the full original file name e.g. 2341D.34B.tif
-            original_path: A string representing the original files relative path e.g. /Ext HD/
-            commons_fname: A string representing the file name after adaptation to Wikimedia Commons conventions.
-
-        """
-    def __init__(self):
-        self.original_fname = original_fname
-        self.original_path = original_path
-        self.commons_fname = None # Set after initiation by create_commons_filename()
+import json
 
 
-    def create_commons_filename(self):
-        """Does what is says """
-        # TODO: use line 88 in https://github.com/lokal-profil/BatchUploadTools/blob/master/batchupload/helpers.py [Issue: https://github.com/mattiasostmar/SMVK-Cypern_2017-01/issues/8]
-        # For Commons conventions see bit.ly/commons_fname_conventions
-
+def load_json_metadata(infile):
+    """Load metadata json blob created with ´metadata_to_json_and_fnamesmap.py´"""
+    metadata = json.load(open("SMVK-Cypern_2017-01_metadata.json"))
 
 def main():
     """Illustrate function-level docstring.
@@ -47,7 +25,6 @@ def main():
     duplicated into the comments (that would be difficult to maintain).
     All subsequent paragraphs in a docstring are indented exactly the same as
     the summary line. The same applies to the closing quotation marks.
-    
     """
 
 
