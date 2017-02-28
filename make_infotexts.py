fbf819dcde399d32e7f90e3e4fc02c011184696f
mattiasostmar <mattiasostmar@gmail.com>
2017-02-28 12:02:14 +0100

    New file for class-based handling of infotexts creation

---
+++ make_infotexts.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding = utf-8 -*-
+
+import json
+import re
+import pandas as pd
+import batchupload.helpers as helpers
+
+people_mapping_file = open("./people_mappings.json")
+people_mapping = json.loads(people_mapping_file.read())
+
+class CypernBatch(object)
+
+class CypernItem(object)
+
+    def __init__(self, ):
