c26715e62695742ec996cff522eb91f4d06c4473
Mattias Östmar <mattiasostmar@gmail.com>
2017-05-11 14:23:35 +0200

    Ensure Nicosiavägen in <Beskrivning> get category:Nicosia

--- create_infotexts.py
+++ create_infotexts.py
@@ -406,7 +406,8 @@ class CypernImage:
             place_matches = []
             for place in places_mapping:
                 if place in desc_string:
-                    place_matches.append(place)
+                    if not re.search(r"Nicosiavägen", desc_string, re.IGNORECASE):
+                        place_matches.append(place)
 
             if len(place_matches) == 1:
                 place = place_matches[0]
