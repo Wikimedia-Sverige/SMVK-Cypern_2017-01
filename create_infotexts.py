4aa8246fe9f9774c38dd6db36349407a5e768b2a
Mattias Östmar <mattiasostmar@gmail.com>
2017-05-15 16:35:02 +0200

    Use an en-dash, not a hyphen, in 1927-1931

--- create_infotexts.py
+++ create_infotexts.py
@@ -121,11 +121,11 @@ def generate_infobox_template(item, img, places_mapping):
     if item["Beskrivning"]:
         infobox += img.data["enriched_description"]
     else:
-        infobox += "Svenska Cypernexpeditionen 1927-1931"  # Generates six cases only
+        infobox += "Svenska Cypernexpeditionen 1927–1931"  # Generates six cases only
         img.meta_cats.append("Media_contributed_by_SMVK_with_poor_description")
 
     infobox += "}}\n"
-    infobox += "{{en|The Swedish Cyprus expedition 1927-1931}}"
+    infobox += "{{en|The Swedish Cyprus expedition 1927–1931}}"
     infobox += "\n"
 
     infobox += "| depicted people    = " + img.data["depicted_people"] + "\n"
@@ -133,7 +133,7 @@ def generate_infobox_template(item, img, places_mapping):
     infobox += "| depicted place     = " + img.data["depicted_place"] + "\n"
 
     infobox += "| date               = "
-    if not item["Fotodatum"] or item["Fotodatum"] == "1927-1931":
+    if not item["Fotodatum"] or item["Fotodatum"] == "1927–1931":
         infobox += "{{Between|1927|1931}}"
     else:
         infobox += str(item["Fotodatum"])
@@ -269,7 +269,7 @@ class CypernImage:
 
         # Ensure first descriptive part is not empty
         if not fname_desc.strip():
-            fname_desc = "Svenska Cypernexpeditionen 1927-1931"
+            fname_desc = "Svenska Cypernexpeditionen 1927–1931"
 
         fname_desc = fname_desc.replace("svenska cypernexpeditionen", "Svenska Cypernexpeditionen")
 
@@ -463,7 +463,7 @@ class CypernImage:
 
         # Step 1 in enrichment process
 
-        description += " Svenska Cypernexpeditionen 1927-1931. "
+        description += " Svenska Cypernexpeditionen 1927–1931. "
 
         # step 2 in enrichment process
         description += self.process_region_addition_to_description(
